729d08e1170bbcf87d51a4be500e7a1a6784e27a
Cossio Carina <Cossio Carina@LAPTOP-ODGO697K>
2023-06-12 17:59:46 -0300

    FinalEj3

---
+++ TPNº6_Ej4/src/tpnº6_ej4/Comparable.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package tpnº6_ej4;
+
+
+public interface Comparable {
+    
+    
+    public int compareTo(Object obj);
+}
