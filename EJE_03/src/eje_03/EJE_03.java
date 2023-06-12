bba8a1c23ec8f4c6f1adfa32e278bb7d3431c660
Sans <Sans@Sans-PC>
2023-06-12 16:23:03 -0300

    Primera Version

--- EJE_03/src/eje_03/EJE_03.java
+++ EJE_03/src/eje_03/EJE_03.java
@@ -5,6 +5,8 @@
  */
 package eje_03;
 
+import java.util.TreeSet;
+import java.util.Scanner;
 /**
  *
  * @author Sans
@@ -16,6 +18,21 @@ public class EJE_03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        TreeSet <String> lista=new TreeSet();
+        Scanner teclado=new Scanner(System.in);
+        int n;
+        System.out.println("Ingrese la cantidad de cadenas a ingresar");
+        n=teclado.nextInt();
+        String c=teclado.nextLine();
+        for(int i=0;i<n;i++){
+            System.out.println("Ingrese cadena");
+            String m=teclado.nextLine();
+            lista.add(m);
+        }
+        for(String i:lista){
+            System.out.println(i);
+        }
+        //el hecho de que lo ordena se debe a la naturaleza del treeset que a medida que se ingresan dato/objetos este los ordena
     }
     
 }
