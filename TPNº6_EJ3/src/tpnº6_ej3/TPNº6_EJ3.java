99bd222e56a1bba4bfb22242fbbb502f890640bf
Cossio Carina <Cossio Carina@LAPTOP-ODGO697K>
2023-06-12 16:27:23 -0300

    FinalEj3

--- TPNº6_EJ3/src/tpnº6_ej3/TPNº6_EJ3.java
+++ TPNº6_EJ3/src/tpnº6_ej3/TPNº6_EJ3.java
@@ -4,11 +4,26 @@
  */
 package tpnº6_ej3;
 
+import java.util.*;
+
 
 public class TPNº6_EJ3 {
 
     
     public static void main(String[] args) {
+     //instanciar TreeSet de cadenas   
+     TreeSet<String> lista = new TreeSet<>(); 
+     
+     //agregar cadenas a la lista
+     lista.add("Jonatan");
+     lista.add("Alexis");
+     lista.add("Cossio");
+     lista.add("Carrizo");
+     
+     //recorrer el conjunto y mostrarlos 
+     for (String cadena : lista){
+         System.out.println(cadena);
+     }
         
     }
     
